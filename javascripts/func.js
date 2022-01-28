ec9e5ff4798497a111a9a26a20f1fba6dccf0af5
Daya Yadav <dayay@Dayas-MacBook-Air.local>
2022-01-28 14:12:26 +0530

    Projects are added

--- javascripts/func.js
+++ javascripts/func.js
@@ -1,7 +1,18 @@
-var x =5; 
-function getname1()
-{
-    return x * x; 
+// var x =5; 
+// function getname1()
+// {
+//     return x * x; 
+// }
+// var k = getname1();
+// console.log(k);
+
+
+function person(firstname,lastname,dob){
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.dob = dob; 
 }
-var k = getname1();
-console.log(k);
+
+const person1 = person('daya', 'yadav', '22-01-2000');
+
+console.log(person1);
